8331a8d91ccde9a7f9ce6ec8bbfb3b0b8729362f
victorsilvestre001-ui <victor.silvestre001@gmail.com>
2026-08-21 03:08:41 -0300

    Specify algorithm for JWT verification

--- authMiddleware.js
+++ authMiddleware.js
@@ -10,7 +10,7 @@ function authMiddleware(req, res, next) {
     const token = authHeader.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET, { algorithms: ['HS256'] });
         req.user = decoded;
         next();
     } catch (err) {
